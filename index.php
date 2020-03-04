4f35599734431154dcd11db836fe0e1716e3e069
Eduardo Andrade <edu.pedro.andrade@gmail.com>
2020-03-04 08:39:21 -0300

    Including admin area

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -16,6 +17,13 @@ $app->get('/', function(){
     //echo json_encode($results);
 
     $page = new Page();
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function(){
+
+    $page = new PageAdmin();
     $page->setTpl("index");
 
 });
