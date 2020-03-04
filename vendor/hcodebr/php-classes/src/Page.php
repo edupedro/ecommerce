ddf92b9b64eb542a9a2df833a0fcaae23e000ab6
Eduardo Andrade <edu.pedro.andrade@gmail.com>
2020-03-04 09:46:57 -0300

    Starting session

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,7 +8,11 @@ class Page{
 
     private $tpl;
     private $options = [];
-    private $defaults = [ "data" => [] ];
+    private $defaults = [ 
+        "header" => true,
+        "footer" => true,
+        "data" => [] 
+    ];
    
     public function __construct($opts = array(), $tpl_dir = "/views/"){
 
@@ -28,7 +32,9 @@ class Page{
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if($this->options["header"] === true){
+            $this->tpl->draw("header");
+        }
     }
 
     private function setData($data = array()){
@@ -43,7 +49,9 @@ class Page{
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if($this->options["footer"] === true){
+            $this->tpl->draw("footer");
+        }
     }
 
 }
